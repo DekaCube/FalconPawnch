8f4f738a159882a74d349498d3d9fbff58bb8887
DekaCube <46388874+DekaCube@users.noreply.github.com>
2020-03-24 22:14:39 -0400

    Create delete_all_users.php

---
+++ admin_tools/delete_all_users.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(0);
+/* DELETE ALL USERS */
+
+require_once './creds.php';
+
+$getaccess = $_REQUEST["pw"];
+
+define("PASSWORD_TO_DELETE","c21fafb3cae0523191c40641820cc2bcec0bf3f03dd82a9b61a5cca254312684");
+define("SALT_FOR_PW","WhenDiagramKings");
+
+
+
+$hashedpw = hash("sha256",$getaccess.SALT_FOR_PW);
+//echo $hashedpw;
+$bomb = "DELETE FROM USERS;";
+
+if($hashedpw == PASSWORD_TO_DELETE){
+    $db->query($bomb);
+    echo "DONE";
+}
+else{
+    echo "PASSWORD FAILURE";
+}
+    
