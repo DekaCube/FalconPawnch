6f4375dbd4b7ed805927a10efbcfc808eaf84df6
DekaCube <46388874+DekaCube@users.noreply.github.com>
2020-03-27 18:52:13 -0400

    Update register.js

--- front_end_examples/login/register.js
+++ front_end_examples/login/register.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     //Set event listeners/handlers for buttons
     document.getElementById('go').onclick = register_user;
+    document.getElementById('login').onclick = login_user;
     
 });
 
@@ -27,6 +28,38 @@ var register_user = function(){
         }
     };
     var p1 = 'https://lamp.cse.fau.edu/~dbenne11/whendiagram/add_user.php?username='
+    var p2 = document.getElementById('uname').value;
+    var p3 = '&pw=';
+    var p4 = document.getElementById('pw').value;
+    url = p1 + p2 + p3 + p4;
+    
+        
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+}
+
+
+//Adding code for login here
+
+var login_user = function(){
+    
+    console.log("SENDING REQUEST FOR NEW LOGIN");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let result = JSON.parse(this.responseText);
+            if(result['login'] == 'success'){
+                document.getElementById('status').innerHTML = "LOGIN GOOD, ACCESS TOKEN = " + result.access_token;
+            }
+            else{
+                document.getElementById('status').innerHTML = "LOGIN FAILED";
+            }
+    
+    
+        }
+    };
+    var p1 = 'https://lamp.cse.fau.edu/~dbenne11/whendiagram/login.php?username='
     var p2 = document.getElementById('uname').value;
     var p3 = '&pw=';
     var p4 = document.getElementById('pw').value;
