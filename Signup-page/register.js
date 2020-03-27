e965317c68a988d9ebb16cfe181f6b97f38b1dd4
Guppies1 <44448374+Guppies1@users.noreply.github.com>
2020-03-27 16:15:46 -0400

    Add files via upload

---
+++ Signup-page/register.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Do this when DOM is loaded
+
+    //Set event listeners/handlers for buttons
+    document.getElementById('go').onclick = register_user;
+    
+});
+
+
+
+var register_user = function(){
+    
+    console.log("SENDING REQUEST FOR NEW ACCOUNT");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let result = JSON.parse(this.responseText);
+            if(result['account_creation'] == 'success'){
+                document.getElementById('status').innerHTML = "NEW ACCOUNT CREATION SUCCESS";
+            }
+            else{
+                document.getElementById('status').innerHTML = result.reason;
+            }
+    
+    
+        }
+    };
+    var p1 = 'https://lamp.cse.fau.edu/~dbenne11/whendiagram/add_user.php?username='
+    var p2 = document.getElementById('uname').value;
+    var p3 = '&pw=';
+    var p4 = document.getElementById('pw').value;
+    url = p1 + p2 + p3 + p4;
+    
+        
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+}
