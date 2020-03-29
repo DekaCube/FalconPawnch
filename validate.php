946a65bd227c636c1268c2e8068d1b8c5f7fc5dd
DekaCube <46388874+DekaCube@users.noreply.github.com>
2020-03-28 22:32:19 -0400

    Create validate.php

---
+++ validate.php
@@ -0,0 +1,65 @@
+<?php
+
+function validate_token($db,$debug_level){
+    //Make sure access token variable is set
+    if(isset($_REQUEST["at"]))
+    {
+        $token = $_REQUEST["at"];
+    }
+    else{
+        return array(0,"null");
+    }
+    //Make sure the token is not null
+    if($token == ""){
+        return array(0,"null");
+    }
+    $request_time = time();
+    $token_valid = 0;
+    $user = "null";
+    
+    
+    $query = "SELECT * FROM TOKENS WHERE token='".$token."';";
+    if($debug_level == 1){
+        echo "BEFORE QUERY<br>";
+    }
+    
+    //echo $token;
+    if($result = $db->query($query)){
+        if($debug_level == 1){
+            echo "STARTING QUERY<br>";
+        }
+        while($row = $result->fetch_assoc())
+        { //Search through all the returned tuples.
+            if($debug_level == 1){
+            echo "INSIDE WHILE<br>";
+            }
+            if($row['token'] == $token) //Make sure the token matches
+            {
+               if($debug_level == 1){
+               echo "INSIDE FIRST IF<br>";
+               }
+                if(($row['t_last_used'] + 1800) >= $request_time) //Make sure token isn't too old
+                {
+                    if($debug_level == 1){
+                        echo "INSIDE SECOND IF<br>";
+                    }
+                    $token_valid = 1;
+                    $user = $row['user'];               
+                }
+            }
+        } //END WHILE
+    }
+    if($debug_level == 1){
+        echo "AFTER QUERY<br>";
+    }
+    if($token_valid == 0){
+        return array(0,"null");
+    }
+    else{
+        $query = "UPDATE TOKENS SET t_last_used='".$request_time."' WHERE token='".$token."';";
+        
+        $db->query($query);
+    }
+    
+    return array(1,$user);
+}
