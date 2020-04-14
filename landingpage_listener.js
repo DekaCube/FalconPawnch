fe1d5dfbad846c549281a05d6e98529c8a402d63
Guppies1 <44448374+Guppies1@users.noreply.github.com>
2020-04-14 14:50:24 -0400

    Add files via upload

--- landingpage_listener.js
+++ landingpage_listener.js
@@ -1,6 +1,4 @@
-var groupname
-
-
+var group = "";
 document.addEventListener("DOMContentLoaded", function(){
     //Do this when DOM is loaded
 
@@ -9,17 +7,140 @@ document.addEventListener("DOMContentLoaded", function(){
     
     document.getElementById('create').onclick = group_handler;
 
-    document.getElementById('leave').onclick = group_handler;
+    document.getElementById('join').onclick = join_handler;
+    
+    
+    display_name();
+    
+    
+    Session.getInfo(groupInfoS,groupInfoF);
+
+    Session.getGroupInfo("123321321",groupinfoSuccess,failed);
 
     
-    Session.getInfo(userinfoSuccess,failed);
     
-
-    Session.getGroupInfo(group,groupinfoSuccess,failed);
-
-    var groupmonday = GroupInfo.arrToString(GroupInfo.getMonday());
+    
 
 });
+
+function null_callback(){
+}
+
+function leaveGroupClick(){
+    Session.leaveGroup(this.id,null_callback,null_callback());
+    Session.getInfo(groupInfoS,groupInfoF);
+    
+    
+}
+
+function join_handler(){
+    let join = document.getElementById("groupname2").value;
+    Session.joinGroup(join,success,failed);
+}
+
+function hideInfoClick(){
+    this.innerHTML = "Info of selected day ";
+    document.getElementById('buts').style.display = "block";
+    let thingtoremove = document.getElementById('groupInfo');
+    document.getElementById('display').removeChild(thingtoremove);
+    this.onclick = showInfoClick;
+    
+}
+
+function buildIframe(){
+    let iframe = document.createElement('iframe');
+    if (day == 'monday'){
+    iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getMonday()));
+    }
+    else if (day == 'tuesday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getTuesday()));
+    }
+    else if (day == 'wednesday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getWednesday()));
+    }
+    else if (day == 'thursday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getThursday()));
+    }
+    else if (day == 'friday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getFriday()));
+    }
+    else if (day == 'saturday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getSaturday()));
+    }
+    else if (day == 'sunday'){
+        iframe.setAttribute("src",'https://lamp.cse.fau.edu/~dbenne11/whendiagram/drawgraph.php?data=' + GroupInfo.arrToString(GroupInfo.getSunday()));
+    }
+    iframe.setAttribute("id","groupInfo");
+    iframe.style.width = "50%";
+    iframe.style.height = "80%";
+    iframe.allow = "fullscreen";   
+    let display = document.getElementById("display");
+    display.appendChild(iframe);
+}
+    
+
+function showInfoClick(){
+    let group = this.id.substring(1);
+    this.innerHTML = "HIDE INFO"
+    let buttons = document.getElementById('buts');
+    buttons.style.display = "none";
+    this.onclick = hideInfoClick;
+    Session.getGroupInfo(group,buildIframe,null_callback);
+    
+    
+}
+
+function populateGroups(){
+    console.log("ENTERING POPULATE GROUPS");
+    
+    let workspace = document.getElementById('grouppanel');
+    workspace.innerHTML = ""; //KILL THAT CONTENT
+    
+    let node = document.createElement('h3');
+    node.innerHTML = "GROUPS";
+    
+    workspace.appendChild(node);
+    
+    let size = UserInfo.groups.length;
+    console.log(UserInfo.groups);
+    if(size == 0){
+        node = document.createElement('h3');
+        node.innerHTML = "NOT A MEMBER IN ANY GROUPS!";
+        workspace.appendChild(node);
+    }
+    let i = 0;
+    let b1 = null;
+    let b2 = null;
+    for(i = 0; i < size;i++){
+        node = document.createElement('p');
+        node.innerHTML = UserInfo.groups[i];
+        console.log(UserInfo.groups[i]);
+        node.setAttribute('id',UserInfo.groups[i]);
+        b1 = document.createElement('button');
+        b1.innerHTML = "leave"; //NEEDS AN ONCLICK
+        b2 = document.createElement('button');
+        b1.setAttribute("id",UserInfo.groups[i]);
+        b1.onclick = leaveGroupClick;
+        b2.innerHTML = "Info of selcted day" //NEEDS AN ONCLICK;
+        let junkspace = document.createElement('br');
+        b2.setAttribute("id","b" + UserInfo.groups[i]);
+        b2.onclick = showInfoClick;
+        workspace.appendChild(node);
+        workspace.appendChild(b1);
+        workspace.appendChild(b2);
+        
+    }
+    console.log("EXITING POPULATE GROUPS");
+}
+
+function groupInfoS(){
+    populateGroups()
+   
+}
+
+function groupInfoF(){
+    alert("SOMETHING WENT WRONG!");
+}
 
 
 
@@ -33,15 +154,15 @@ function group_handler(){
     Session.createGroup(group,success,failed);
 }
 
-function leave_handler(){
-    Session.leaveGroup("123321321",leaveSucess,failed)
+function display_name(){
+    Session.getInfo(userinfoSuccess,failed);
 }
 
 
 function success(){
     console.log(Session.access_token);
     console.log("worked");
-    alert("login sucess");
+    alert("success");
 
 }
 
@@ -52,20 +173,11 @@ function failed(){
 function userinfoSuccess(){
     console.log(Session.access_token);
     console.log("worked");
-    console.log(UserInfo.getUsername());
-    groupname = UserInfo.groups[0];
-    
-    alert(groupname);
- 
-}  
+    group = UserInfo.groups[0];
+}
 
 function groupinfoSuccess(){
     console.log(Session.access_token);
     console.log("group retrived");
     console.log(GroupInfo.arrToString(GroupInfo.getMonday()));
-}
-
-function leaveSucess(){
-    console.log(Session.access_token);
-    console.log("left group");
 }
